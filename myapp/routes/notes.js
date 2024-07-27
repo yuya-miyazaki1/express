45f179645f895d024e9f37c407a6a78599c6098f
yuya-miyazaki1 <70211382+yuya-miyazaki1@users.noreply.github.com>
2024-07-28 03:13:40 +0900

    Update notes.js

--- myapp/routes/notes.js
+++ myapp/routes/notes.js
@@ -14,3 +14,22 @@ const note = await notes.findOne(query);
 res.json(note);
 })
 module.exports = router;
+
+//第6回にてroutes配下に追加した
+/**
+var express = require('express');
+var router = express.Router();
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://cu2101125497:dai8kai@cluster0.a6hmhrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+res.json(note);
+})
+module.exports = router;*/
