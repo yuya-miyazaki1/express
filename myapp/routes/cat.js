a4757988295939e3ac24bc17da215f10d25fee46
yuya-miyazaki1 <70211382+yuya-miyazaki1@users.noreply.github.com>
2024-07-27 23:08:54 +0900

    Create cat.js

---
+++ myapp/routes/cat.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+router.get('/', async (req, res) => {
+request('https://api.thecatapi.com/v1/images/search', function (error, response, 
+body) {
+if (!error && response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+}
+});
+})
+module.exports = router;
+
+//第8回でroutes配下に追加した
